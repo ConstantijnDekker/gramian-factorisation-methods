9da27010fa9eb3144ce09dc080e0c2b48c12f90c
ConstantijnDekker <88208502+ConstantijnDekker@users.noreply.github.com>
2023-08-09 10:29:18 +0200

    Create alterProjIncremental.jl

---
+++ alterProjIncremental.jl
@@ -0,0 +1,21 @@
+function polar(A)
+    (U, _, V) = LA.svd(A)
+    U * V'
+end
+
+function alterProjIncremental(C, tolerance, maxiters)
+    Q = LA.I
+    V = LA.I
+    X = Q*C
+    iters = 0
+    #error = [max(0, -minimum(X))]
+    while minimum(X) < -tolerance && iters < maxiters
+        Y = map(x -> max(x, 0), X)
+        V = polar(Y * X')
+        X = V*X
+        Q = V*Q
+        iters += 1
+        #push!(error, max(0, -minimum(X)))
+    end
+    Q, iters#, error
+end
